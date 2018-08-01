11c7ffe5c3e73f50df428443aff200a8d89c1bb3
tonyn4444 <tntuyen@ucdavis.edu>
2018-08-01 12:36:11 -0700

    add particles js background

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Particles from 'react-particles-js';
+
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 
+
+const particlesOptions = {
+	particles: {
+		number: {
+			value: 30,
+			density: {
+				enable: true,
+				value_area: 800
+			}
+		}
+	}
+}
+
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+      	<Particles 
+          params={particlesOptions}
+          className='particles'
+        />
         <Navigation />
         <Logo />
         <Rank />
